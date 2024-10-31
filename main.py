abd50d6d8dce5950f47380d060105e58bb692de9
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-10-31 17:35:38 +0000

    Base todo list
    Started the base of the todo list
    - Asks for user input
    - Takes users reply, trims and lowers it
    - Adds items to a list and recalls them with a number attached

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-userInput = input("Enter a todo: ")
-print(userInput)
+todoList = []
+
+while True:
+    userAction = input("Type add, show or exit: ")
+    match userAction.lower().strip():
+        case "add":
+            userInput = input("Enter a todo: ")
+            capitalised = userInput.title()
+            todoList.append(capitalised)
+        case "show" | "display":
+            currentPos = 1
+            for item in todoList:
+                print(currentPos, "-", item)
+                currentPos = currentPos + 1
+        case "exit":
+            break
+        case _:
+            print("You entered and unknown command, please enter a valid command.")
+
+print("Thank you. Goodbye")
