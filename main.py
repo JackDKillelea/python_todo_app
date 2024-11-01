619148cdf7ebe6abc7b07a810df717fddf1d875f
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-11-01 16:56:33 +0000

    Changed logic
    - Updated from match case to if statements, because I wanted to make it more suitable
    - Leant how to properly use file open and close

--- main.py
+++ main.py
@@ -1,66 +1,61 @@
 print("Hello, thank you for using my todo list!")
 
 while True:
-    userAction = input("Type add, show, edit, delete, complete or exit: ")
-    match userAction.lower().strip():
-        case "add":
-            userInput = input("Enter a todo: ") + "\n"
-            # Read what is currently in the file and create a list from it
-            todoFile = open("files/todos", "r")
-            todoList = todoFile.readlines()
-            todoFile.close()
+    userAction = input("Type add, show, edit, complete or exit: ")
+    userAction = userAction.lower().strip()
 
-            capitalised = userInput.title()
-            todoList.append(capitalised)
-            # Writes data to a local text file
-            todoFile = open("files/todos", "w")
+    if 'add' in userAction or "new" in userAction:
+        userInput = userAction[4:]
+        # Read what is currently in the local text file and create a list from it
+        with open("files/todos") as todoFile:
+            todoList = todoFile.readlines()
+
+        capitalised = userInput.title()
+        todoList.append(capitalised)
+
+        # Writes data to a local text file
+        with open("files/todos", "w") as todoFile:
             todoFile.writelines(todoList)
-            todoFile.close()
 
-        case "show" | "display":
-            todoFile = open("files/todos", "r")
+    elif userAction == "show":
+        with open("files/todos") as todoFile:
             todoList = todoFile.readlines()
-            todoFile.close()
 
-            if not todoList:
-                print("You have no current todos.")
-            else:
-                for index, item in enumerate(todoList):
-                    row = f"{index + 1}. {item}"
-                    print(row.replace("\n", ""))
+        if not todoList:
+            print("You have no current todos.")
+        else:
+            # List Comprehension
+            # formattedTodos = [item.strip("\n") for item in todoList]
+            for index, item in enumerate(todoList):
+                row = f"{index + 1}. {item}"
+                print(row.strip("\n"))
 
-        case "edit":
-            todoFile = open("files/todos", "r")
+    elif "edit" in userAction:
+        with open("files/todos") as todoFile:
             todoList = todoFile.readlines()
-            todoFile.close()
 
-            edit = int(input("Please enter the number of the item you'd like to edit: "))
-            newTodo = input("Please enter updated todo: ")
-            todoList[edit - 1] = newTodo
+        userInput = userAction[5:]
+        print(f"You are editing: {todoList[int(userInput) - 1]}")
+        newTodo = input("Please enter updated todo: ") + "\n"
+        todoList[int(userInput.strip()) - 1] = newTodo.title()
 
-            todoFile = open("files/todos", "w")
+        with open("files/todos", "w") as todoFile:
             todoFile.writelines(todoList)
-            todoFile.close()
 
-        case "delete" | "complete":
-            todoFile = open("files/todos", "r")
+    elif "complete" in userAction:
+        with open("files/todos") as todoFile:
             todoList = todoFile.readlines()
-            todoFile.close()
 
-            if userAction.lower().strip() == "complete":
-                todo = int(input("Please enter the number of the item you'd like to complete: "))
-            else:
-                todo = int(input("Please enter the number of the item you'd like to delete: "))
-            todoList.pop(todo - 1)
+        userInput = userAction[9:]
+        todoList.pop(int(userInput.strip()) - 1)
 
-            todoFile = open("files/todos", "w")
+        with open("files/todos", "w") as todoFile:
             todoFile.writelines(todoList)
-            todoFile.close()
 
-        case "exit":
-            break
+    elif userAction == "exit":
+        break
 
-        case _:
-            print("You entered and unknown command, please enter a valid command.")
+    else:
+        print("You entered and unknown command, please enter a valid command.")
 
 print("Thank you. Goodbye")
