5477e0e872680778c56cb3c92bbca9e1e2cb6c88
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-11-01 13:35:10 +0000

    Added file management
    - Added file management so it saves and reads from a file rather than storing in memory

--- main.py
+++ main.py
@@ -1,29 +1,65 @@
-todoList = []
-
 print("Hello, thank you for using my todo list!")
 
 while True:
     userAction = input("Type add, show, edit, delete, complete or exit: ")
     match userAction.lower().strip():
         case "add":
-            userInput = input("Enter a todo: ")
+            userInput = input("Enter a todo: ") + "\n"
+            # Read what is currently in the file and create a list from it
+            todoFile = open("files/todos", "r")
+            todoList = todoFile.readlines()
+            todoFile.close()
+
             capitalised = userInput.title()
             todoList.append(capitalised)
+            # Writes data to a local text file
+            todoFile = open("files/todos", "w")
+            todoFile.writelines(todoList)
+            todoFile.close()
+
         case "show" | "display":
-            for index, item in enumerate(todoList):
-                print(f"{index + 1}. {item}")
+            todoFile = open("files/todos", "r")
+            todoList = todoFile.readlines()
+            todoFile.close()
+
+            if not todoList:
+                print("You have no current todos.")
+            else:
+                for index, item in enumerate(todoList):
+                    row = f"{index + 1}. {item}"
+                    print(row.replace("\n", ""))
+
         case "edit":
+            todoFile = open("files/todos", "r")
+            todoList = todoFile.readlines()
+            todoFile.close()
+
             edit = int(input("Please enter the number of the item you'd like to edit: "))
             newTodo = input("Please enter updated todo: ")
             todoList[edit - 1] = newTodo
+
+            todoFile = open("files/todos", "w")
+            todoFile.writelines(todoList)
+            todoFile.close()
+
         case "delete" | "complete":
+            todoFile = open("files/todos", "r")
+            todoList = todoFile.readlines()
+            todoFile.close()
+
             if userAction.lower().strip() == "complete":
                 todo = int(input("Please enter the number of the item you'd like to complete: "))
             else:
                 todo = int(input("Please enter the number of the item you'd like to delete: "))
-            todoList.remove(todoList[todo - 1])
+            todoList.pop(todo - 1)
+
+            todoFile = open("files/todos", "w")
+            todoFile.writelines(todoList)
+            todoFile.close()
+
         case "exit":
             break
+
         case _:
             print("You entered and unknown command, please enter a valid command.")
 
