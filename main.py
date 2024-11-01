1fc7f7413a02c2f48cc9d75fb3db233e9bd491a6
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-11-01 17:44:42 +0000

    Add Error Handling
    Added error handling to edit and complete
    - Handles index errors
    - Handles value errors

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+from ast import Index
+
 print("Hello, thank you for using my todo list!")
 
 while True:
     userAction = input("Type add, show, edit, complete or exit: ")
     userAction = userAction.lower().strip()
 
-    if 'add' in userAction or "new" in userAction:
+    if userAction.startswith("add") or userAction.startswith("new"):
         userInput = userAction[4:]
         # Read what is currently in the local text file and create a list from it
         with open("files/todos") as todoFile:
             todoList = todoFile.readlines()
 
         capitalised = userInput.title()
-        todoList.append(capitalised)
+        todoList.append(capitalised + "\n")
 
         # Writes data to a local text file
         with open("files/todos", "w") as todoFile:
             todoFile.writelines(todoList)
 
-    elif userAction == "show":
+    elif userAction.startswith("show"):
         with open("files/todos") as todoFile:
             todoList = todoFile.readlines()
 
@@ -30,29 +32,43 @@ while True:
                 row = f"{index + 1}. {item}"
                 print(row.strip("\n"))
 
-    elif "edit" in userAction:
-        with open("files/todos") as todoFile:
-            todoList = todoFile.readlines()
+    elif userAction.startswith("edit"):
+        try:
+            with open("files/todos") as todoFile:
+                todoList = todoFile.readlines()
 
-        userInput = userAction[5:]
-        print(f"You are editing: {todoList[int(userInput) - 1]}")
-        newTodo = input("Please enter updated todo: ") + "\n"
-        todoList[int(userInput.strip()) - 1] = newTodo.title()
+            userInput = userAction[5:]
+            print(f"You are editing: {todoList[int(userInput) - 1]}")
+            newTodo = input("Please enter updated todo: ") + "\n"
+            todoList[int(userInput.strip()) - 1] = newTodo.title()
 
-        with open("files/todos", "w") as todoFile:
-            todoFile.writelines(todoList)
+            with open("files/todos", "w") as todoFile:
+                todoFile.writelines(todoList)
+        except ValueError:
+            print("Please enter a number when using edit.")
+            continue
+        except IndexError:
+            print("That todo does not exist, please use the show command to see what todos you have.")
+            continue
 
-    elif "complete" in userAction:
-        with open("files/todos") as todoFile:
-            todoList = todoFile.readlines()
+    elif userAction.startswith("complete"):
+        try:
+            with open("files/todos") as todoFile:
+                todoList = todoFile.readlines()
 
-        userInput = userAction[9:]
-        todoList.pop(int(userInput.strip()) - 1)
+            userInput = userAction[9:]
+            todoList.pop(int(userInput.strip()) - 1)
 
-        with open("files/todos", "w") as todoFile:
-            todoFile.writelines(todoList)
+            with open("files/todos", "w") as todoFile:
+                todoFile.writelines(todoList)
+        except ValueError:
+            print("Please enter a number when using complete.")
+            continue
+        except IndexError:
+            print("That todo does not exist, please use the show command to see what todos you have.")
+            continue
 
-    elif userAction == "exit":
+    elif userAction.startswith("exit"):
         break
 
     else:
